a26f747405c8828cac3120dab71e705ff1f0927c
Katrina Owen <katrina.owen@gmail.com>
2014-01-13 11:56:45 -0700

    Add  and  assertions

--- lib/minitest/homework.rb
+++ lib/minitest/homework.rb
@@ -5,5 +5,33 @@ module Minitest
     def self.runnable_methods
       methods_matching(/^test_/)
     end
+
+    def __
+      :blank
+    end
+
+    def assert_equal(actual, expected)
+      if actual == __
+        begin
+          raise
+        rescue => e
+          failure = Exception.new("Fill in the blank, then re-run the test.")
+          failure.set_backtrace e.backtrace[1..-1]
+          raise failure
+        end
+      else
+        super
+      end
+    end
+
+    def maybe(boolean)
+      begin
+        raise
+      rescue => e
+        failure = Exception.new("Replace `maybe` with either `assert` or `refute`, then re-run the test.")
+        failure.set_backtrace e.backtrace[1..-1]
+        raise failure
+      end
+    end
   end
 end
